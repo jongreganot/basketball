c22d9ee5db8becdc3ca6e43a29ef29143c8159fe
Joshua Reganot <joshuaeli.reganot@merkle.com>
2024-03-19 14:41:34 +0800

    initial commit

---
+++ src/pages/Game.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+class Game extends React.Component {
+    constructor(props) {
+       super(props);
+    }
+
+    render () {
+        return (
+            <div className="container">
+                
+            </div>
+        )
+    };
+}
+
+export default Game;
